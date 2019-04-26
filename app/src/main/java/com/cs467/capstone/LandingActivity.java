d57b998cf81288d2f2162749238ac8ee1a594f37
Merry Purser <purser.merry@gmail.com>
2019-04-26 12:27:16 -0600

    Made first login button on landing page, no functionality.

--- app/src/main/java/com/cs467/capstone/LandingActivity.java
+++ app/src/main/java/com/cs467/capstone/LandingActivity.java
@@ -5,15 +5,31 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import com.example.cs467.capstone.R;
 
 public class LandingActivity extends AppCompatActivity {
+    // TODO perhaps this can be a local variable
+    private View mLandingFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        mLandingFormView = findViewById(R.id.landing_view);
+
+        Button mLogInButton = (Button) findViewById(R.id.log_in_button);
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+
+        /*
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +41,14 @@ public class LandingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+         */
+    }
+
+    /**
+     * Go to Login page
+     */
+    private void goToLogin() {
+
     }
 
 }
