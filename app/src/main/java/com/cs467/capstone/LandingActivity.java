bfff1721a204d99fa93c0a5275ba9d19cf99b38f
Merry Purser <purser.merry@gmail.com>
2019-04-26 12:28:38 -0600

    cleanup unused code

--- app/src/main/java/com/cs467/capstone/LandingActivity.java
+++ app/src/main/java/com/cs467/capstone/LandingActivity.java
@@ -1,11 +1,7 @@
 package com.cs467.capstone;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import com.example.cs467.capstone.R;
@@ -29,19 +25,7 @@ public class LandingActivity extends AppCompatActivity {
             }
         });
 
-        /*
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-         */
     }
 
     /**
